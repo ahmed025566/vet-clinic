d5004d7daab177b2b06eafe1e5ab1a52a1899c73
ahmed025566 <ahmedsalah025566@gmail.com>
2023-07-17 23:56:33 +0300

    Create data base and insert date and query data

---
+++ schema.sql
@@ -0,0 +1,14 @@
+-- First thing : I wrote the following command to use sql commands : 
+sudo su - postgres
+-- then I created my database using the following command : 
+createdb vit_clinic
+-- THNE I got access to my data base using the following command : 
+psql vit_clinic
+-- And Finally I created a table using the following command : 
+CREATE TABLE animals( id                INT,
+                      name              VARCHAR(100),   
+                      date_of_birth     DATE,  
+                      escape_attempts   INT,   
+                      neutered          BOOLEAN,    
+                      weight_kg         FLOAT      
+                    );
