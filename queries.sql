29f6509f4721bcffb1f80336dd6d1255e00aae85
ahmed025566 <ahmedsalah025566@gmail.com>
2023-07-18 15:47:00 +0300

    Add quries to extract and to update some date

--- queries.sql
+++ queries.sql
@@ -21,3 +21,48 @@ SELECT * FROM animals WHERE name != 'Gabumon';
 
 -- The following command is for listing animals where weight is >= 10.4 kg and less than or equal to 17.3kg
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+-- The following commands of for updating species field and then rollback: 
+BEGIN;
+UPDATE animals  SET species = 'unspecified';
+ROLLBACK;
+
+-- The following command is for updateing species column based on some conditions:
+BEGIN;
+UPDATE animals   SET species = 'digimon' WHERE name LIKE '%mon';
+UPDATE animals   SET species = 'pokemon' WHERE species = '';
+COMMIT;
+
+-- The following command is for deleting everything and then back to the original table: 
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
+
+-- The following command is for making savepoints and then roll back to it;
+BEGIN;
+DELETE FROM animals WHERE date_of_birth > '2022-01-01';
+SAVEPOINT sp1;
+UPDATE aniamls SET weight_kg = weight_kg * -1;
+ROLLBACK TO sp1;
+UPDATE aniamls SET weight_kg = weight_kg * -1 WHERE weight_kg < 0;
+COMMIT;
+
+-- The following commands is for answring query questions : 
+
+-- How many animals are there ?
+SELECT COUNT(name) FROM animals;
+
+-- How many animals never tried to escape:
+SELECT COUNT(name) FROM aniamls WHERE escape_attempts = 0;
+
+-- What is the average weight of animals?
+SELECT AVG(weight_kg) FROM aniamls;
+
+-- Who escapes the most, neutered or not neutered animals?
+SELECT name, neutered FROM animals WHERE escape_attempts = ( SELECT MAX(escape_attempts) FROM animals);
+
+-- What is the minimum and maximum weight of each type of animal?
+SELECT species, MAX(weight_kg), MIN(weight_kg) FROM animals GROUP BY species;
+
+--What is the average number of escape attempts per animal type of those born between 1990 and 2000?
+SELECT AVG(escape_attempts) FROM aniamls WHERE (EXTRACT(YEAR FROM CAST(date_of_birth AS DATE))) BETWEEN 1990 AND 2000;
